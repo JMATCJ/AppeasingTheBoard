04d7d7918c10ae0a1250d8ddec3880b8bbe72319
Trevor Good <tdgood@users.noreply.github.com>
2022-04-02 00:43:09 -0400

    Added meters

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-import pygame
 import sys
 from pathlib import Path
+from typing import Tuple
+
+import pygame
 from pygame.locals import QUIT
 
 # Check if we are in a pyinstaller "onefile" binary. Different path prefix in that case:
@@ -10,11 +12,29 @@ else:
     ASSETS_DIR = Path("assets")
 
 
+class Meter(pygame.sprite.Sprite):
+    def __init__(self, meter_pos: Tuple[int, int], meter_color: Tuple[int, int, int]):
+        super().__init__()
+        self.surf = pygame.Surface((300, 25))
+        self.surf.fill(meter_color)
+        self.rect = self.surf.get_rect(topleft=meter_pos)
+
+
 # Main
 pygame.init()
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instantiate the Meter
+company_cash_meter = Meter((0, 0), (60, 182, 252))
+employee_morale_meter = Meter((0, 50), (98, 242, 72))
+employee_productivity_meter = Meter((0, 100), (239, 62, 62))
+company_reputation_meter = Meter((0, 150), (240, 247, 46))
+
+# Create meter group
+meters = pygame.sprite.Group()
+meters.add(company_cash_meter, employee_morale_meter, employee_productivity_meter, company_reputation_meter)
 
 running = True
 while running:
@@ -27,14 +47,10 @@ while running:
     # TODO
 
     # Drawing
-    # Draw everything white
-    screen.fill((255, 255, 255))
-    # Create a small black square
-    surf = pygame.Surface((50, 50))
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
-    rect.top = 100
-    rect.left = 100
-    # Draw ("blit") the surface to the screen, position it with the rect
-    screen.blit(surf, rect)
+    # Draw everything gray
+    screen.fill((100, 100, 100))
+    # Draw ("blit") the meters to the screen, position it with the rect
+    for meter in meters:
+        screen.blit(meter.surf, meter.rect)
+
     pygame.display.flip()
